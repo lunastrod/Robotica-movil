47aceea39901fb4f47f80dc57d79cf7354a0154a
danparseg <daniel.parra.segovia@gmail.com>
2022-11-21 10:55:59 +0100

    obstacle funciona, vibraciones

--- P3/obstacle.py
+++ P3/obstacle.py
@@ -34,52 +34,50 @@ def VFF_controller(laser, goal):
     """
     #PARAMETERS
     obstacle_k = 0.01
-    goal_k = 4
-    ignore_dist = 10#m
-    max_goal_force = 5
-    max_obstacle_force = 45090000000000
+    goal_k = 6
+    max_goal_force = 2
+    
+    front_k=2
 
-
-    ignore_dist*=ignore_dist
     obstacle_force = [0, 0]
     goal_force = [0, 0]
     for i in range(len(laser)):
-        #if the distance to the obstacle is more than ignore_dist, then the force is 0
-        if(abs(laser[i][0])+abs(laser[i][1])>ignore_dist):
-            continue
         #the force is inversely proportional to the square of the distance to the obstacle
         #calculate the distance to the obstacle
         sqr_dist=laser[i][0]*laser[i][0]+laser[i][1]*laser[i][1]
         #update the obstacle force
-        obstacle_force[0]=obstacle_force[0]-laser[i][0]/sqr_dist*obstacle_k
-        obstacle_force[1]=obstacle_force[1]-laser[i][1]/sqr_dist*obstacle_k
-
-    obstacle_force[0]=min(max(obstacle_force[0],-max_obstacle_force),max_obstacle_force)
-    obstacle_force[1]=min(max(obstacle_force[1],-max_obstacle_force),max_obstacle_force)
+        if(i>len(laser)*0.3 and i<len(laser)*0.7):
+            obstacle_force[0]=obstacle_force[0]+laser[i][0]/sqr_dist*obstacle_k*front_k
+            obstacle_force[1]=obstacle_force[1]+laser[i][1]/sqr_dist*obstacle_k*front_k
+        else:
+            obstacle_force[0]=obstacle_force[0]+laser[i][0]/sqr_dist*obstacle_k
+            obstacle_force[1]=obstacle_force[1]+laser[i][1]/sqr_dist*obstacle_k
 
     #update the goal force
-    goal_force[0]=min(max_goal_force,goal_force[0]+goal[0]*goal_k)
-    goal_force[1]=min(max_goal_force,goal_force[1]+goal[1]*goal_k)
+    goal_mod=math.sqrt(goal[0]*goal[0]+goal[1]*goal[1])
+    unit_goal=[goal[0]/goal_mod,goal[1]/goal_mod]
+    goal_strength=min(max_goal_force,goal_k*goal_mod)
+    goal_force=[unit_goal[0]*goal_strength,unit_goal[1]*goal_strength]
     return obstacle_force,goal_force,tuple(map(sum, zip(obstacle_force, goal_force)))
 
 def force_to_vw(force):
     """
     This function converts the control signal (force) into the desired linear and angular velocities of the robot.
     """
-    maxv=0
+    maxv=5
     maxw=math.pi/2
+    kv=4
+    kw=10
 
     angle=math.atan2(force[1],force[0])
     #print("prev angle",math.degrees(angle))
-    angle=(angle-math.pi/2)
-    if(angle<-math.pi):
-        angle=angle+2*math.pi
-    print("angle",math.degrees(angle),angle)
-
+    print("angle",math.degrees(angle))
     #v=math.cos(angle)*maxv
-    v=max(0,-1.62*angle**2+1)*maxv
+    #v=max(0,-1.62*angle**2+1)*maxv
+    v=maxv/(1+kv*angle**2)
     #w=math.sin(angle)*maxw
-    w=max(-maxw,min(maxw,3.2*angle**3))
+    w=max(-maxw,min(maxw,kw*angle**3))
+    print("force",force,"translates to v,w",v,w)
     return v, w
 
 
@@ -129,28 +127,22 @@ while True:
     if(is_close(local_target)):
         currentTarget.setReached(True)
         
-    print("local target: ", local_target)
     # Parse the laser data
     laser=vectorize_laser(parse_laser_data(HAL.getLaserData()))
     #print(laser)
     # Calculate the control signal
     obstacle_force,goal_force,force=VFF_controller(laser,local_target)
-    print("obstacle_force: ",obstacle_force)
-    print("goal_force: ",goal_force)
-    print("force: ",force)
     robotv, robotw = force_to_vw(force)
-    print("v: ", robotv)
-    print("w: ", robotw)
-    print()
     # Send the velocity to the robot
     HAL.setW(robotw)
     HAL.setV(robotv)
     # Update the GUI
-    GUI.showForces(obstacle_force, goal_force, force)
+    GUI.showForces(goal_force, obstacle_force, force) #green, red, black
     # show image
     GUI.showImage(HAL.getImage())
     # show target
     GUI.showLocalTarget(target)
+    
     
     
     
